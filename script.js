343f9a3674508f1e50713221e2aa76e4853715b0
imenturki <68286731+Imenturki0@users.noreply.github.com>
2023-11-15 16:44:07 +0300

    extra requirements done

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
+let seconds = 0;
+let minutes = 0;
+let timerInterval;
+let timerRunning = false;
+
+function updateTimerDisplay() {
+    document.getElementById('seconds').textContent = seconds < 10 ? '0' + seconds : seconds;
+    document.getElementById('minutes').textContent = minutes < 10 ? '0' + minutes : minutes;
+}
+
 document.getElementById('startButton').addEventListener('click', function() {
-    let seconds = 0;
-    let minutes = 0;
-    setInterval(function() {
-        seconds++;
-        if (seconds == 60) {
-            minutes++;
-            seconds = 0;
-        }
-        document.getElementById('seconds').textContent = seconds < 10 ? '0' + seconds : seconds;
-        document.getElementById('minutes').textContent = minutes < 10 ? '0' + minutes : minutes;
-    }, 1000);
+    if (!timerRunning) {
+        timerInterval = setInterval(function() {
+            seconds++;
+            if (seconds == 60) {
+                minutes++;
+                seconds = 0;
+            }
+            updateTimerDisplay();
+        }, 1000);
+        timerRunning = true;
+    }
+});
+
+document.getElementById('pauseButton').addEventListener('click', function() {
+    if (timerRunning) {
+        clearInterval(timerInterval);
+        timerRunning = false;
+    } else {
+        timerInterval = setInterval(function() {
+            seconds++;
+            if (seconds == 60) {
+                minutes++;
+                seconds = 0;
+            }
+            updateTimerDisplay();
+        }, 1000);
+        timerRunning = true;
+    }
 });
